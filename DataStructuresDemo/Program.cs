6fbf36c8be3b571c3edeafb95bebf96d56dda01a
Shrikant <sawantshrikant07@gmail.com>
2023-03-21 13:51:46 +0530

    [Shrikant] add:LinkedList

--- DataStructuresDemo/Program.cs
+++ DataStructuresDemo/Program.cs
@@ -7,6 +7,12 @@ namespace DataStructuresDemo
         private static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Data Structures Problem!");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            Console.WriteLine("Nodes in the list:");
+            list.Display();
         }
     }
 }
