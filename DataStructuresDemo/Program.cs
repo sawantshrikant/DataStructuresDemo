ae354294e740835ac783137c11142f0310c7449b
Shrikant <sawantshrikant07@gmail.com>
2023-03-21 13:14:01 +0530

    [Shrikant] add:WelcomeToDataStructuresProblem

---
+++ DataStructuresDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructuresDemo
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Data Structures Problem!");
+        }
+    }
+}
